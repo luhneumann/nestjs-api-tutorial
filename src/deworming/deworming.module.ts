a4ba1d289b5cfbfaf4e778b5752300d504868170
Luana Neumann <luanafneumann@hotmail.com>
2023-04-10 11:40:16 -0300

    Config deworming module

--- src/deworming/deworming.module.ts
+++ src/deworming/deworming.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DewormingService } from './deworming.service';
 import { DewormingController } from './deworming.controller';
+import { Deworming, DewormingSchema } from './entities/deworming.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Deworming.name,
+        schema: DewormingSchema
+      }
+    ])
+  ],
   controllers: [DewormingController],
   providers: [DewormingService]
 })
