73473056853999f758ff291bd643a9e301f6855f
Luana Neumann <luanafneumann@hotmail.com>
2023-04-10 17:48:16 -0300

    animal death entity

--- src/animal-deaths/entities/animal-death.entity.ts
+++ src/animal-deaths/entities/animal-death.entity.ts
@@ -1,1 +1,22 @@
-export class AnimalDeath {}
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose, { Date } from "mongoose";
+import { Animal } from "src/animals/entities/animal.entity";
+import { User } from "src/user/entities/user.entities";
+
+@Schema({versionKey: false})
+export class AnimalDeath extends Document{
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref:User.name , required: false })
+    user: User
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref:Animal.name , required: false })
+    animal: Animal
+
+    @Prop({ type: Date, required: false})
+    date: Date
+    
+    @Prop({ type: String, required: false})
+    reason: string
+
+    @Prop({ type: String, required: false})
+    observation: string    
+}
