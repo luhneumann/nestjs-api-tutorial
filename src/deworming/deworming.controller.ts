1d0d91b4a2351041f104009bf2686b9e94f9bf15
Luana Neumann <luanafneumann@hotmail.com>
2023-04-10 11:30:40 -0300

    deworming services and controllers

--- src/deworming/deworming.controller.ts
+++ src/deworming/deworming.controller.ts
@@ -1,34 +1,90 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { DewormingService } from './deworming.service';
 import { CreateDewormingDto } from './dto/create-deworming.dto';
 import { UpdateDewormingDto } from './dto/update-deworming.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ListDewormingDto } from './dto/list-deworming.dto';
 
+@ApiTags('deworming')
 @Controller('deworming')
 export class DewormingController {
   constructor(private readonly dewormingService: DewormingService) {}
 
   @Post()
-  create(@Body() createDewormingDto: CreateDewormingDto) {
-    return this.dewormingService.create(createDewormingDto);
+  @ApiResponse({
+    status: 200,
+    description: 'Cria um objeto',
+    type: CreateDewormingDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma resposta de erro sobre os dados enviados'
+  })
+  @ApiOperation({ summary: 'Deworming - Cria um novo registro de vermifugação'})
+  async create(@Body() createDewormingDto: CreateDewormingDto) {
+    return await this.dewormingService.create(createDewormingDto);
   }
 
   @Get()
-  findAll() {
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna uma lista de objetos',
+    type: ListDewormingDto,
+    isArray: true
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma resposta de erro sobre os dados enviados'
+  })
+  @ApiOperation({ summary: 'Deworming - Lista os registros de vermifugação'})
+  async findAll() {
     return this.dewormingService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.dewormingService.findOne(+id);
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna um objeto',
+    type: ListDewormingDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma resposta de erro sobre os dados enviados'
+  })
+  @ApiOperation({ summary: 'Deworming - retorna um registro de vermifugação'})  
+  async findOne(@Param('id') id: string) {
+    return await this.dewormingService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDewormingDto: UpdateDewormingDto) {
-    return this.dewormingService.update(+id, updateDewormingDto);
+  @Put(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Edita um objeto',
+    type: UpdateDewormingDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma resposta de erro sobre os dados enviados'
+  })
+  @ApiOperation({ summary: 'Deworming - Edita um registro de vermifugação'})
+  async update(@Param('id') id: string, @Body() updateDewormingDto: UpdateDewormingDto) {
+    return await this.dewormingService.update(id, updateDewormingDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dewormingService.remove(+id);
+  @ApiResponse({
+    status: 200,
+    description: 'Deleta um objeto',   
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma resposta de erro sobre os dados enviados'
+  })
+  @ApiOperation({ summary: 'Deworming - Lista os registros de vermifugação'})
+  async remove(@Param('id') id: string) {
+    return await this.dewormingService.remove(id);
   }
 }
--- src/deworming/deworming.service.ts
+++ src/deworming/deworming.service.ts
@@ -1,26 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDewormingDto } from './dto/create-deworming.dto';
 import { UpdateDewormingDto } from './dto/update-deworming.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Deworming } from './entities/deworming.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class DewormingService {
-  create(createDewormingDto: CreateDewormingDto) {
-    return 'This action adds a new deworming';
+  constructor(@InjectModel(Deworming.name) private readonly dewormingService: Model<Deworming>){}
+
+  async create(createDewormingDto: CreateDewormingDto) {
+    try {
+      const newDeworming = new this.dewormingService(createDewormingDto)
+      .save()
+      return await newDeworming
+    } catch (error) {
+      console.log(error)
+    }    
   }
 
-  findAll() {
-    return `This action returns all deworming`;
+  async findAll() {
+    try {
+      return await this.dewormingService
+      .find()
+      .exec()
+    } catch (error) {
+      console.log(error)
+    }  
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} deworming`;
+  async findOne(id: string) {
+    try{
+      return await this.dewormingService
+      .findById(id)
+      .exec()
+    } catch (error){
+      console.log(error)
+    }    
   }
 
-  update(id: number, updateDewormingDto: UpdateDewormingDto) {
-    return `This action updates a #${id} deworming`;
+  async update(id: string, updateDewormingDto: UpdateDewormingDto) {
+    try {
+
+      const updateDeworming = await this.dewormingService
+      .findByIdAndUpdate({_id: id}, updateDewormingDto)
+      .exec()
+      return updateDeworming
+      
+    } catch (error) {
+      console.log(error)
+    }    
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} deworming`;
+  async remove(id: string) {
+    try {
+      return await this.dewormingService
+      .findByIdAndDelete(id)
+      .exec()
+    } catch (error) {
+      console.log(error)
+    }    
   }
 }
