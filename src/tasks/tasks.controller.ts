f6808c4b8024e45cb755fb3cda8b59527fc3971e
unknown <luanafneumann@hotmail.com>
2023-04-13 09:29:44 -0300

    tasks module, service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,34 +1,93 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ListAbortionDto } from 'src/abortion/dto/list-abortion.dto';
+import { ListTaskDto } from './dto/list-task.dto';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
+  @ApiResponse({
+    status: 200,
+    description: 'Cria um objeto',
+    type: CreateTaskDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma mensagem de erro sobre oi dados enviados'  
+  })
+  @ApiOperation({ summary: 'Tasks - Cria uma nova tarefa'})
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return await this.tasksService.create(createTaskDto);
   }
 
   @Get()
-  findAll() {
-    return this.tasksService.findAll();
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna uma lista de objetos',
+    type: ListAbortionDto,
+    isArray: true
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma mensagem de erro sobre oi dados enviados'  
+  })
+  @ApiOperation({ summary: 'Tasks - Retorna uma lista de tarefas'})
+  async findAll() {
+    return await this.tasksService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(+id);
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna um objeto',
+    type: ListTaskDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma mensagem de erro sobre oi dados enviados'  
+  })
+  @ApiOperation({ summary: 'Tasks - Retorna uma tarefa'})
+  async findOne(@Param('id') id: string) {
+    return await this.tasksService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
+  @Put(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Edita um objeto',
+    type: UpdateTaskDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma mensagem de erro sobre oi dados enviados'  
+  })
+  @ApiOperation({ summary: 'Tasks - Edita uma tarefa'})
+  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return await this.tasksService.update(id, updateTaskDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+  @ApiResponse({
+    status: 200,
+    description: 'Remove um objeto',
+    type: UpdateTaskDto,
+    isArray: false
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna uma mensagem de erro sobre oi dados enviados'  
+  })
+  @ApiOperation({ summary: 'Tasks - Remove uma tarefa'})
+  async remove(@Param('id') id: string) {
+    return await this.tasksService.remove(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './entities/task.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(@InjectModel(Task.name) private readonly taskModel: Model<Task>){}
+
+  async create(createTaskDto: CreateTaskDto) {
+    try {
+      const newTask = await new this.taskModel(createTaskDto).save()
+      return newTask
+    } catch (error) {
+      console.log(error)
+    }   
   }
 
-  findAll() {
-    return `This action returns all tasks`;
+  async findAll() {
+    try {
+      return await this.taskModel.find().exec()      
+    } catch (error) {
+      console.log(error)
+    }   
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: string) {
+    try {
+      return await this.taskModel.findById(id).exec()      
+    } catch (error) {
+      console.log(error)
+    }   
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: string, updateTaskDto: UpdateTaskDto) {
+    try {
+      return await this.taskModel
+      .findByIdAndUpdate({_id: id}, updateTaskDto)
+      .exec()      
+    } catch (error) {
+      console.log(error)
+    }   
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: string) {
+    try {
+      return await this.taskModel
+      .findByIdAndDelete(id)
+      .exec()      
+    } catch (error) {
+      console.log(error)
+    }   
   }
 }
